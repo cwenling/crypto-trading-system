cabd3dc90da69c49dd93b7e0dde3dce68d3033cc
cwenling <e0425906@u.nus.edu>
2021-05-30 01:37:07 +0800

    RiskWatcher implements EventListener

--- src/main/java/spark/logic/algo/RiskWatcher.java
+++ src/main/java/spark/logic/algo/RiskWatcher.java
@@ -1,4 +1,28 @@
 package spark.logic.algo;
 
-public class RiskWatcher {
+import spark.data.CachedOrderBook;
+import spark.logic.message.EventListener;
+import spark.logic.schedule.ScheduledEvent;
+
+public class RiskWatcher implements EventListener, Runnable {
+    private double imbalance;
+
+    public RiskWatcher() {
+        this.imbalance = 0;
+    }
+
+    @Override
+    public void handleEvent(CachedOrderBook orderBook) {
+
+    }
+
+    @Override
+    public void handleEvent(ScheduledEvent scheduledEvent) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
